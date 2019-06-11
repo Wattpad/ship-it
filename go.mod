cf87dbf42e20a898950cfa14299f1fe8ad67e543
Ethan Peterson <ethan.peterson@wattpad.com>
2019-06-11 10:29:03 -0400

    sarts SQS consumer code for ecr events
    VELO-1405

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module ship-it
 
 go 1.12
 
-require github.com/go-chi/chi v4.0.2+incompatible
+require (
+	github.com/Wattpad/sqsconsumer v0.0.0-20170510132609-fa099c630d1b
+	github.com/aws/aws-sdk-go v1.19.47
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/golang/mock v1.3.1 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20190607181551-461777fb6f67 // indirect
+)
--- internal/sqs/sqs.go
+++ internal/sqs/sqs.go
@@ -1,6 +1,47 @@
 package ecrconsumer
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/Wattpad/sqsconsumer"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
 type SQSConfig struct {
 	Name   string
 	Region string
+	Svc    *sqs.SQS
+}
+
+func NewSQSConfig(name string, region string) *SQSConfig {
+	conf := &aws.Config{
+		Region: &region,
+	}
+	svc := sqs.New(session.New(conf))
+
+	return &SQSConfig{
+		Name:   name,
+		Region: region,
+		Svc:    svc,
+	}
+}
+
+func (s *SQSConfig) NewSQSConsumer() (*sqsconsumer.Consumer, error) {
+	// Create SQS service
+	service, err := sqsconsumer.SQSObjectForQueue(s.Name, s.Svc)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create and return SQS consumer
+	consumer := sqsconsumer.NewConsumer(service, processMessage)
+	return consumer, nil
+}
+
+func processMessage(ctx context.Context, msg string) error {
+	fmt.Println(msg)
+	return nil
 }
