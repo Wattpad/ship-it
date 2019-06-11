db0629321f4b0d80b76317e91109368688210622
Ethan Peterson <ethan.peterson@wattpad.com>
2019-06-11 15:31:02 -0400

    add test code to main.go

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"ship-it/internal/api"
+	ecrconsumer "ship-it/internal/sqs"
 )
 
 func main() {
 	http.ListenAndServe(":80", api.New())
+
+	// ECR SQS Consumer Setup
+	conf, err := ecrconsumer.NewSQSConfig("kube-deploy-events.fifo", "us-east-1")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	consumer, err := conf.NewSQSConsumer()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	ecrconsumer.RunConsumer(consumer)
 }
