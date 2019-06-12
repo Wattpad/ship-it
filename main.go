6f707769c75471b0c8714857b2d5c595b52867ab
Ethan Peterson <ethan.peterson@wattpad.com>
2019-06-12 10:28:31 -0400

    init logger in main.go
    VELO-1405

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"ship-it/internal/api"
 	ecrconsumer "ship-it/internal/sqs"
+
+	kitlog "github.com/go-kit/kit/log"
 )
 
 func main() {
 	http.ListenAndServe(":80", api.New())
-
 	// ECR SQS Consumer Setup
 	conf, err := ecrconsumer.NewSQSConfig("kube-deploy-events.fifo", "us-east-1")
 	if err != nil {
@@ -17,7 +19,9 @@ func main() {
 		return
 	}
 
-	consumer, err := conf.NewSQSConsumer()
+	logger := kitlog.NewJSONLogger(kitlog.NewSyncWriter(os.Stdout))
+
+	consumer, err := conf.NewSQSConsumer(logger)
 	if err != nil {
 		log.Println(err)
 		return
