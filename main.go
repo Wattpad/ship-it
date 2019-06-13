389947f4471ff21330be0704b3f149f59d7d0c09
Chris Dueck <chris.dueck@wattpad.com>
2019-06-13 11:51:47 -0400

    Add service lifetime context (#9)
    * Add service lifetime context
    
    Add a service context to control the lifetime of the service's
    subsystems. Includes termination signal handling and logging.
    
    * Remember to shutdown the http server as well

--- main.go
+++ main.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"ship-it/internal/api"
+
+	"github.com/go-kit/kit/log"
 )
 
 func main() {
-	http.ListenAndServe(":80", api.New())
+	logger := log.NewJSONLogger(log.NewSyncWriter(os.Stdout))
+	logger = log.With(logger, "timestamp", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+
+	logger.Log("event", "service.start")
+	defer logger.Log("event", "service.stop")
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signals := make(chan os.Signal, 2)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-signals
+		cancel()
+	}()
+
+	srv := http.Server{
+		Addr:    ":80",
+		Handler: api.New(),
+	}
+
+	exit := make(chan error)
+	go func() { exit <- srv.ListenAndServe() }()
+
+	select {
+	case <-ctx.Done():
+		logger.Log("event", "service.exit", "error", ctx.Err())
+	case err := <-exit:
+		cancel()
+		logger.Log("event", "service.exit", "error", err)
+	}
+
+	srv.Shutdown(context.Background())
 }
