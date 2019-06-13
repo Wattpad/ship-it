5fc5768ee93a0a1ead92ab5713a68acf435139f9
Ethan Peterson <ethan.peterson@wattpad.com>
2019-06-13 11:30:17 -0400

    create json template for grid tile data
    VELO-1428

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module ship-it
 
 go 1.12
 
-require github.com/go-chi/chi v4.0.2+incompatible
+require (
+	github.com/VividCortex/gohistogram v1.0.0 // indirect
+	github.com/Wattpad/sqsconsumer v0.0.0-20190611184259-511082fa45b3 // indirect
+	github.com/aws/aws-sdk-go v1.19.49 // indirect
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-kit/kit v0.8.0 // indirect
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/mock v1.3.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20190611141213-3f473d35a33a // indirect
+)
--- web/src/App.js
+++ web/src/App.js
@@ -19,6 +19,30 @@ var data = [ // old image data (this json will be restructured with kube deploy 
   { 'img': 'http://i.imgur.com/rCrvQTv.jpg', 'link': 'https://www.instagram.com/p/BQ6_Wa2gmdR/', 'title': 'Dubai Desert Conservation Reserve', 'description': 'Photo by @christopheviseux / Early morning flight on a hot air balloon ride above the Dubai Desert Conservation Reserve. Merely an hour drive from the city, the park was created to protect indigenous species and biodiversity. The Arabian Oryx, which was close to extinction, now has a population well over 100. There are many options to explore the desert and flying above may be one of the most mesmerizing ways. Follow me @christopheviseux for more images from the Middle East. #dubai #desert' }
 ]
 
+var newData = [
+  {
+    serviceName: "loki",
+    deploymentStatus: "deployed",
+    timeStamp: "5 min ago",
+    chartVersion: "0.1.1",
+    autoDeploy: true,
+    mirandaPR: "https://github.com",
+    highlanderPR: "https://github.com",
+    slackLink: "https://slack.com",
+    dockerLink: "https://www.docker.com/",
+    dataDogDashboard: "dd",
+    dataDogMonitor: "",
+    sumoLogs: "",
+    travisBuild: "",
+    kubeResources: [
+      "pod 1",
+      "pod 2",
+      "pod 3",
+      "pod 4",
+    ]
+  }
+]
+
 function App() {
   var dataString = JSON.stringify(data)
   return (
