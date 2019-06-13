42b4026f011e9b1172427d462e730730eff322cd
Ethan Peterson <ethan.peterson@wattpad.com>
2019-06-13 12:04:28 -0400

    save changes before branch switch

--- web/src/App.js
+++ web/src/App.js
@@ -40,7 +40,27 @@ var newData = [
       "pod 3",
       "pod 4",
     ]
-  }
+  }, {
+    serviceName: "hermes",
+    deploymentStatus: "deployed",
+    timeStamp: "5 min ago",
+    chartVersion: "0.1.1",
+    autoDeploy: true,
+    mirandaPR: "https://github.com",
+    highlanderPR: "https://github.com",
+    slackLink: "https://slack.com",
+    dockerLink: "https://www.docker.com/",
+    dataDogDashboard: "dd",
+    dataDogMonitor: "",
+    sumoLogs: "",
+    travisBuild: "",
+    kubeResources: [
+      "pod 1",
+      "pod 2",
+      "pod 3",
+      "pod 4",
+    ]
+  },
 ]
 
 function App() {
