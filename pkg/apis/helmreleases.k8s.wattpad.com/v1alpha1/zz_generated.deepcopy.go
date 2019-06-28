e46fb17219f14e1f69a7807090804af0155aabf9
Chris Dueck <chris.dueck@wattpad.com>
2019-06-28 11:35:45 -0400

    update generated deepcopy code

--- pkg/apis/helmreleases.k8s.wattpad.com/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/helmreleases.k8s.wattpad.com/v1alpha1/zz_generated.deepcopy.go
@@ -72,7 +72,7 @@ func (in *HelmRelease) DeepCopyObject() runtime.Object {
 func (in *HelmReleaseList) DeepCopyInto(out *HelmReleaseList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]HelmRelease, len(*in))
