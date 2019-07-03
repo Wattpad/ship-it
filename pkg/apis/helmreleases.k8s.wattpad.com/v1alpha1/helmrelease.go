4a7a9e8c815d9ef0c899cdaa8183ab827cb6920b
Chris Dueck <chris.dueck@wattpad.com>
2019-07-03 17:22:32 -0400

    Fixup HelmRelease serialization
    The `HelmRelease.Spec.Values` needs to work with k8s code-gen tools and
    be properly encoded/decoded from YAML. `unstructured.Unstructured` isn't
    suitable because it's intended for k8s API objects, which our `Values`
    field is not.
    
    We need to implement a `HelmValues.DeepCopyInto` method to support
    code-gen, and marshal the `HelmRelease` as yaml using json marshalling,
    as the default yaml serialization doesn't respect JSON struct tags.

--- pkg/apis/helmreleases.k8s.wattpad.com/v1alpha1/helmrelease.go
+++ pkg/apis/helmreleases.k8s.wattpad.com/v1alpha1/helmrelease.go
@@ -1,15 +1,19 @@
 package v1alpha1
 
 import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/helm/pkg/chartutil"
 )
 
 // HelmReleaseSpec defines the desired state of HelmRelease
 type HelmReleaseSpec struct {
-	ReleaseName string                    `json:"releaseName"`
-	Chart       ChartSpec                 `json:"chart"`
-	Values      unstructured.Unstructured `json:"values"`
+	ReleaseName string     `json:"releaseName"`
+	Chart       ChartSpec  `json:"chart"`
+	Values      HelmValues `json:"values"`
 }
 
 // HelmReleaseSpec defines the desired Helm chart
@@ -22,6 +26,42 @@ type ChartSpec struct {
 // HelmReleaseStatus defines the observed state of HelmRelease
 type HelmReleaseStatus struct {
 	// TODO
+}
+
+func (r HelmRelease) MarshalYAML() (interface{}, error) {
+	rawJSON, err := json.Marshal(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to marshal HelmRelease into JSON")
+	}
+
+	var jsonObj interface{}
+	if err := json.Unmarshal(rawJSON, &jsonObj); err != nil {
+		return nil, errors.Wrap(err, "unable to unmarshal HelmRelease from JSON")
+	}
+
+	return jsonObj, err
+}
+
+// +k8s:deepcopy-gen:interfaces
+// HelmValues allows us to implement runtime.Object for map[string]interface type.
+type HelmValues chartutil.Values
+
+func (in *HelmValues) DeepCopyInto(out *HelmValues) {
+	if in == nil {
+		return
+	}
+
+	b, err := yaml.Marshal(in)
+	if err != nil {
+		return
+	}
+
+	var values chartutil.Values
+	if err := yaml.Unmarshal(b, &values); err != nil {
+		return
+	}
+
+	*out = HelmValues(values)
 }
 
 // +genclient
