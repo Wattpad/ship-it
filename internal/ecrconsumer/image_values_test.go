8b1399f48d9072abe41258bb8ee6f5888d92caf0
Ethan Peterson <ethan.peterson@wattpad.com>
2019-07-05 17:08:37 -0400

    move deep copy and serialization tests to v1alpha1

--- internal/ecrconsumer/image_values_test.go
+++ internal/ecrconsumer/image_values_test.go
@@ -6,51 +6,8 @@ import (
 	"ship-it/pkg/apis/k8s.wattpad.com/v1alpha1"
 
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-const crYaml = `apiVersion: helmreleases.k8s.wattpad.com/v1alpha1
-kind: HelmRelease
-metadata:
-  creationTimestamp: null
-  name: example-microservice
-spec:
-  chart:
-    path: microservice
-    repository: wattpad.s3.amazonaws.com/helm-charts
-    revision: HEAD
-  releaseName: example-release
-  values:
-    autoscaler:
-      maxPods: 50
-      minPods: 30
-      targetCPUUtilizationPercent: 60
-    containerPort: 80
-    cronjob:
-      closeoutAppLabel: loki-closeout
-      image:
-        repository: 723255503624.dkr.ecr.us-east-1.amazonaws.com/kube-tools
-        tag: deda27d
-      schedule: 0 0 * * *
-    image:
-      repository: 723255503624.dkr.ecr.us-east-1.amazonaws.com/loki
-      tag: cc064f8a3d3fa0fe938e95d961ad0278770fa5d2
-    microservice:
-      nameOverride: loki
-    nodePort: 31828
-    resources:
-      limits:
-        cpu: 500m
-        memory: 256Mi
-      requests:
-        cpu: 500m
-        memory: 128Mi
-    securityContext:
-      privileged: true
-    serviceAccountName: loki
-    servicePort: 80
-status: {}
-`
 
 func TestGetImagePath(t *testing.T) {
 	var tests = []struct {
@@ -309,17 +266,39 @@ func TestParseImage(t *testing.T) {
 
 func TestWithImage(t *testing.T) {
 	expectedImg := Image{
-		Registry:   "723255503624.dkr.ecr.us-east-1.amazonaws.com",
-		Repository: "loki",
+		Registry:   "bar",
+		Repository: "foo",
 		Tag:        "new-tag",
 	}
 
-	rls, err := LoadRelease([]byte(crYaml))
-	assert.NoError(t, err)
+	rls := v1alpha1.HelmRelease{
+		metav1.TypeMeta{
+			Kind:       "HelmRelease",
+			APIVersion: "apiVersion: helmreleases.k8s.wattpad.com/v1alpha1",
+		},
+		metav1.ObjectMeta{
+			Name: "example-microservice",
+		},
+		v1alpha1.HelmReleaseSpec{
+			ReleaseName: "example-release",
+			Chart: v1alpha1.ChartSpec{
+				Repository: "wattpad.s3.amazonaws.com/helm-charts",
+				Path:       "microservice",
+				Revision:   "HEAD",
+			},
+			Values: map[string]interface{}{
+				"image": map[string]interface{}{
+					"repository": "bar/foo",
+					"tag":        "baz",
+				},
+			},
+		},
+		v1alpha1.HelmReleaseStatus{},
+	}
 
-	outputRls := WithImage(expectedImg, *rls)
+	outputRls := WithImage(expectedImg, rls)
 
-	path := getImagePath(outputRls.Spec.Values, "loki")
+	path := getImagePath(outputRls.Spec.Values, "foo")
 	if path == nil {
 		t.Fatal("no matching image found")
 	}
@@ -344,58 +323,4 @@ func TestStringMapCleanup(t *testing.T) {
 		},
 	}
 	assert.Equal(t, expectedMap, cleanUpStringMap(inputMap))
-}
-
-func TestDeepCopy(t *testing.T) {
-	tests := []struct {
-		original         v1alpha1.HelmValues
-		transformer      func(v v1alpha1.HelmValues) v1alpha1.HelmValues
-		expectedCopy     v1alpha1.HelmValues
-		expectedOriginal v1alpha1.HelmValues
-	}{
-		// reassignment
-		{
-			original: nil,
-			transformer: func(v v1alpha1.HelmValues) v1alpha1.HelmValues {
-				return v1alpha1.HelmValues{}
-			},
-			expectedCopy:     v1alpha1.HelmValues{},
-			expectedOriginal: nil,
-		},
-		// mutation
-		{
-			original: v1alpha1.HelmValues{},
-			transformer: func(v v1alpha1.HelmValues) v1alpha1.HelmValues {
-				v["foo"] = "bar"
-				return v
-			},
-			expectedCopy:     v1alpha1.HelmValues{"foo": "bar"},
-			expectedOriginal: v1alpha1.HelmValues{},
-		},
-		{
-			original: v1alpha1.HelmValues{"foo": v1alpha1.HelmValues{"bar": "baz"}},
-			transformer: func(v v1alpha1.HelmValues) v1alpha1.HelmValues {
-				v["foo"] = v1alpha1.HelmValues{"bar": "oof"}
-				return v
-			},
-			expectedCopy:     v1alpha1.HelmValues{"foo": v1alpha1.HelmValues{"bar": "oof"}},
-			expectedOriginal: v1alpha1.HelmValues{"foo": v1alpha1.HelmValues{"bar": "baz"}},
-		},
-	}
-	for i, tc := range tests {
-		output := v1alpha1.HelmValues(make(map[string]interface{}))
-		tc.original.DeepCopyInto(&output)
-		assert.Exactly(t, tc.expectedCopy, tc.transformer(output), "copy was not mutated. test case: %d", i)
-		assert.Exactly(t, tc.expectedOriginal, tc.original, "original was mutated. test case: %d", i)
-	}
-}
-
-func TestSerializeRoundTrip(t *testing.T) {
-	rls, err := LoadRelease([]byte(crYaml))
-	assert.NoError(t, err)
-
-	outBytes, err := yaml.Marshal(rls)
-	assert.NoError(t, err)
-
-	assert.Equal(t, crYaml, string(outBytes))
 }
