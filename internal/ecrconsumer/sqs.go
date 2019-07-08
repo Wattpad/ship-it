a3e0b2d6adafb02e4c05653e3d4481b481016cbe
Ethan Peterson <ethan.peterson@wattpad.com>
2019-07-08 11:49:55 -0400

    add helper funcs for getting image from sqs msg

--- internal/ecrconsumer/sqs.go
+++ internal/ecrconsumer/sqs.go
@@ -2,6 +2,8 @@ package ecrconsumer
 
 import (
 	"context"
+	"encoding/json"
+	"strings"
 	"time"
 
 	"github.com/Wattpad/sqsconsumer"
@@ -16,6 +18,27 @@ type GitCommands interface {
 	UpdateFile(msg string, branch string, path string, fileContent []byte) (*github.RepositoryContentResponse, error)
 }
 
+// Will delete after VELO-1453 is merged as that PR already has this type included
+type Image struct {
+	Registry   string
+	Repository string
+	Tag        string
+}
+
+type SQSMessage struct {
+	Detail struct {
+		EventTime        time.Time `json:"eventTime"`
+		ResponseElements struct {
+			Image struct {
+				RepositoryName string `json:"repositoryName"`
+				ImageID        struct {
+					ImageDigest string `json:"imageDigest"`
+				}
+			} `json:"image"`
+		} `json:"responseElements"`
+	} `json:"detail"`
+}
+
 // New returns a SQS consumer which processes ECR PushImage events by updating
 // the associated service's chart values in a remote Git repository.
 func New(logger log.Logger, hist metrics.Histogram, name string, svc sqsconsumer.SQSAPI, client GitCommands) (*sqsconsumer.Consumer, error) {
@@ -26,17 +49,53 @@ func New(logger log.Logger, hist metrics.Histogram, name string, svc sqsconsumer
 
 	track := dataDogTimeTracker(hist)
 	wrappedLogger := loggerMiddleware(logger)
-	handler := middleware.ApplyDecoratorsToHandler(processMessage, track, wrappedLogger)
-
+	handler := middleware.ApplyDecoratorsToHandler(processMessage(client), track, wrappedLogger)
 	consumer := sqsconsumer.NewConsumer(service, handler)
 
 	return consumer, nil
 }
 
-func processMessage(ctx context.Context, msg string) error {
-	return nil
+func parseSHA(digest string) string {
+	arr := strings.Split(digest, ":")
+	return arr[1]
 }
 
+func parseMsg(msg string) (*SQSMessage, error) {
+	js := &SQSMessage{}
+	err := json.Unmarshal([]byte(msg), js)
+	if err != nil {
+		return nil, err
+	}
+	return js, nil
+}
+
+func makeImage(repoName string, tag string) Image {
+	return Image{
+		Registry:   "723255503624.dkr.ecr.us-east-1.amazonaws.com",
+		Repository: repoName,
+		Tag:        tag,
+	}
+}
+
+func processMessage(client GitCommands) sqsconsumer.MessageHandlerFunc {
+	return func(ctx context.Context, msg string) error {
+		// Handle Git Commits Here
+		sqsMessage, err := parseMsg(msg)
+		if err != nil {
+			return err
+		}
+
+		tag := parseSHA(sqsMessage.Detail.ResponseElements.Image.ImageID.ImageDigest)
+		_ = makeImage(sqsMessage.Detail.ResponseElements.Image.RepositoryName, tag)
+
+		return nil
+		// Get CR Bytes
+		// Unmarshal
+		// Attach updated Image
+		// Marshal
+		// Commit new Bytes to GitHub
+	}
+}
 
 func dataDogTimeTracker(hist metrics.Histogram) middleware.MessageHandlerDecorator {
 	return func(fn sqsconsumer.MessageHandlerFunc) sqsconsumer.MessageHandlerFunc {
