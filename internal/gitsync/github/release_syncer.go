001054d92b2a1a58e4f77ab28355103b83284e23
Chris Dueck <chris.dueck@wattpad.com>
2019-07-08 21:29:22 -0400

    Add gitsync/github pkg
    This pkg implements the github event consumer functionality of the
    gitsync component. It consumes push events from Github API which signal
    that a chart has been changed, then syncs a helm release with the new
    chart.
    
    The consumer will be used to keep the ship-it-registry chart
    automatically synced between github and k8s.
    
    VELO-1452

---
+++ internal/gitsync/github/release_syncer.go
@@ -0,0 +1,55 @@
+package github
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+	"k8s.io/helm/pkg/helm"
+	"k8s.io/helm/pkg/proto/hapi/chart"
+	rls "k8s.io/helm/pkg/proto/hapi/services"
+)
+
+type helmClient interface {
+	InstallReleaseFromChart(chart *chart.Chart, namespace string, opts ...helm.InstallOption) (*rls.InstallReleaseResponse, error)
+	UpdateReleaseFromChart(rlsName string, chart *chart.Chart, opts ...helm.UpdateOption) (*rls.UpdateReleaseResponse, error)
+}
+
+type releaseSyncer struct {
+	Namespace string
+	Release   string
+	Timeout   time.Duration
+
+	client helmClient
+}
+
+func newReleaseSyncer(c helmClient, namespace, release string, timeout time.Duration) *releaseSyncer {
+	return &releaseSyncer{
+		Namespace: namespace,
+		Release:   release,
+		Timeout:   timeout,
+		client:    c,
+	}
+}
+
+func (r *releaseSyncer) UpdateOrInstallFromChart(ctx context.Context, chart *chart.Chart) error {
+	timeoutSeconds := int64(timeoutForDeadline(ctx, r.Timeout).Seconds())
+
+	_, err := r.client.UpdateReleaseFromChart(r.Release, chart, helm.UpgradeTimeout(timeoutSeconds))
+	err = errors.Wrap(err, "failed to update release from chart")
+
+	if err != nil {
+		_, err = r.client.InstallReleaseFromChart(chart, r.Namespace, helm.InstallTimeout(timeoutSeconds))
+		err = errors.Wrap(err, "failed to install release from chart")
+	}
+
+	return err
+}
+
+func timeoutForDeadline(ctx context.Context, def time.Duration) time.Duration {
+	if deadline, ok := ctx.Deadline(); ok {
+		return time.Until(deadline)
+	}
+
+	return def
+}
