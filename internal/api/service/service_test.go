5ba0cffa871ed617d1ab8b8070f359b027851091
Ethan Peterson <ethan.peterson@wattpad.com>
2019-07-11 17:29:45 -0400

    create mock git client

--- internal/api/service/service_test.go
+++ internal/api/service/service_test.go
@@ -30,13 +30,28 @@ func newMockK8sClient(name string, time time.Time) *mockK8sClient {
 	}
 }
 
-func TestListReleasesPopulatesFromConfigMap(t *testing.T) {
-	name := "configmap-1"
+type mockGitClient struct {
+	Org string
+	Checks github.ChecksService
+}
+
+func newMockGitClient(org string) *mockGitClient {
+	return &mockGitClient{
+		Org: org,
+	}
+}
+
+func (m *mockGitClient) GetTravisCIBuildURLForRef(ctx context.Context, repo string, ref string) (string, error) {
+	return "www.travisci.com", nil
+}
+
+func TestListReleasesPopulatesFromCustomResource(t *testing.T) {
+	name := "mock-resource"
 	currentTime := time.Now()
 
-	mock := newMockK8sClient(name, currentTime)
-	mockGit := github.New(context.Background(), "Wattpad", "fake")
-	svc := New(mock, mockGit, "miranda", "master")
+	mockK8s := newMockK8sClient(name, currentTime)
+	mockGit := newMockGitClient("wattpad")
+	svc := New(mockK8s, mockGit, "miranda", "master")
 
 	releases, err := svc.ListReleases(context.Background())
 
@@ -44,4 +59,8 @@ func TestListReleasesPopulatesFromConfigMap(t *testing.T) {
 	assert.Len(t, releases, 1)
 	assert.Equal(t, name, releases[0].Name)
 	assert.Equal(t, currentTime, releases[0].Created)
+}
+
+func TestTravisFieldIsPopulated(t *testing.T) {
+
 }
