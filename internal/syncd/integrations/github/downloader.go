4a6f1923668351d4610c00494e18bb207026f4fb
Chris Dueck <chris.dueck@wattpad.com>
2019-07-12 14:06:43 -0400

    Add syncd github integration
    The github package provides an implementation of `syncd.ChartListener`
    and `syncd.ChartReconciler`. The listener produces events for modified
    charts by receiving push events from Github over SQS and reconciles the
    new charts by deploying them via a helm client.
    
    VELO-1452

---
+++ internal/syncd/integrations/github/downloader.go
@@ -0,0 +1,59 @@
+package github
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+	"github.com/pkg/errors"
+	"k8s.io/helm/pkg/chartutil"
+)
+
+type RepositoriesService interface {
+	GetContents(ctx context.Context, org, repo, path string, opts *github.RepositoryContentGetOptions) (*github.RepositoryContent, []*github.RepositoryContent, *github.Response, error)
+}
+
+type downloader struct {
+	Organization string
+	repositories RepositoriesService
+}
+
+func newDownloader(svc RepositoriesService, org string) *downloader {
+	return &downloader{
+		Organization: org,
+		repositories: svc,
+	}
+}
+
+func (d *downloader) BufferDirectory(ctx context.Context, repo, path, ref string) ([]*chartutil.BufferedFile, error) {
+	file, dir, _, err := d.repositories.GetContents(ctx, d.Organization, repo, path, &github.RepositoryContentGetOptions{
+		Ref: ref,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var files []*chartutil.BufferedFile
+
+	if file != nil {
+		content, err := file.GetContent()
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to get file contents")
+		}
+
+		files = append(files, &chartutil.BufferedFile{
+			Name: file.GetName(),
+			Data: []byte(content),
+		})
+	} else {
+		for _, subDir := range dir {
+			subFiles, err := d.BufferDirectory(ctx, repo, subDir.GetPath(), ref)
+			if err != nil {
+				return nil, err
+			}
+
+			files = append(files, subFiles...)
+		}
+	}
+
+	return files, nil
+}
