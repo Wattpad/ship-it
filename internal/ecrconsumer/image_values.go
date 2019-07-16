250f9529c6a0b7caaccf2705c09f925cbb78c0fb
John Houston <john.houston@wattpad.com>
2019-07-16 14:31:09 -0400

    Add operator skeleton using kubebuilder
    VELO-1421

--- internal/ecrconsumer/image_values.go
+++ internal/ecrconsumer/image_values.go
@@ -1,11 +1,12 @@
 package ecrconsumer
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"ship-it/internal"
 
-	"ship-it/pkg/apis/k8s.wattpad.com/v1alpha1"
+	shipitv1beta1 "ship-it-operator/api/v1beta1"
 )
 
 func getImagePath(obj map[string]interface{}, serviceName string) []string {
@@ -96,13 +97,20 @@ func cleanUpMapValue(v interface{}) interface{} {
 	}
 }
 
-func WithImage(img internal.Image, r v1alpha1.HelmRelease) v1alpha1.HelmRelease {
+func WithImage(img internal.Image, r shipitv1beta1.HelmRelease) (shipitv1beta1.HelmRelease, error) {
 	copy := r.DeepCopy()
 
-	cleanMap := cleanUpStringMap(copy.Spec.Values)
-	copy.Spec.Values = v1alpha1.HelmValues(cleanMap)
+	var values map[string]interface{}
+	err := json.Unmarshal(copy.Spec.Values.Raw, &values)
+	if err != nil {
+		return shipitv1beta1.HelmRelease{}, err
+	}
 
-	update(copy.Spec.Values, img)
+	cleanMap := cleanUpStringMap(values)
+	update(cleanMap, img)
 
-	return *copy
+	newJSON, _ := json.Marshal(cleanMap)
+	copy.Spec.Values.Raw = newJSON
+
+	return *copy, nil
 }
