26afdbebd9b2ef6bcc227d0e7ba538539c6fa032
Chris Dueck <chris.dueck@wattpad.com>
2019-08-01 17:13:19 -0400

    router tests take 2

--- internal/api/router_test.go
+++ internal/api/router_test.go
@@ -1,33 +1,34 @@
 package api
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"ship-it/internal/api/models"
 	"testing"
 
-	"github.com/go-chi/chi"
+	"github.com/go-kit/kit/metrics/discard"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRoutes(t *testing.T) {
-	expectedRoutes := map[string]interface{}{
-		"/*":      nil,
-		"/health": nil,
-		"/api/*": map[string]interface{}{
-			"/releases": nil,
-		},
-	}
+type nopService struct{}
 
-	handler := New(nil, nil)
-
-	if routes, ok := handler.(chi.Routes); ok {
-		assertRoutes(t, expectedRoutes, routes)
-	}
+func (m *nopService) ListReleases(ctx context.Context) ([]models.Release, error) {
+	return nil, nil
 }
 
-func assertRoutes(t *testing.T, expected map[string]interface{}, r chi.Routes) {
-	for _, route := range r.Routes() {
-		assert.Contains(t, expected, route.Pattern)
-		if nested, ok := expected[route.Pattern].(map[string]interface{}); ok {
-			assertRoutes(t, nested, route.SubRoutes)
-		}
+func TestRoutes(t *testing.T) {
+	requests := []*http.Request{
+		httptest.NewRequest(http.MethodGet, "/index.html", nil),
+		httptest.NewRequest(http.MethodGet, "/health", nil),
+		httptest.NewRequest(http.MethodGet, "/api/releases", nil),
+	}
+
+	handler := New(&nopService{}, discard.NewHistogram())
+
+	for _, req := range requests {
+		rec := httptest.NewRecorder()
+		handler.ServeHTTP(rec, req)
+		assert.NotEqual(t, rec.Code, http.StatusNotFound)
 	}
 }
