6bfcc54afef454e870e8e6a9e7664857c4e7838c
Chris Dueck <chris.dueck@wattpad.com>
2019-08-01 16:14:33 -0400

    Improve api/middleware/timer test coverage

--- internal/api/middleware/timer_test.go
+++ internal/api/middleware/timer_test.go
@@ -1,10 +1,34 @@
 package middleware
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi"
+	"github.com/go-kit/kit/metrics"
 	"github.com/stretchr/testify/assert"
 )
+
+type mockHistogram struct {
+	labels        []string
+	observeCalled bool
+}
+
+func (m *mockHistogram) Observe(t float64) {
+	m.observeCalled = true
+}
+
+func (m *mockHistogram) With(kvs ...string) metrics.Histogram {
+	m.labels = kvs
+	return m
+}
+
+func withRouteContext(req *http.Request) *http.Request {
+	rctx := chi.NewRouteContext()
+	return req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+}
 
 func TestGetIdentifier(t *testing.T) {
 	var tests = []struct {
@@ -20,4 +44,27 @@ func TestGetIdentifier(t *testing.T) {
 	for _, test := range tests {
 		assert.Equal(test.expected, getIdentifier(test.input))
 	}
+}
+
+func TestTimer(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	var histo mockHistogram
+
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/foo", nil)
+	req = withRouteContext(req)
+
+	Timer(&histo)(handler).ServeHTTP(rec, req)
+
+	t.Run("it adds method and endpoint labels", func(t *testing.T) {
+		assert.Contains(t, histo.labels, "method")
+		assert.Contains(t, histo.labels, "endpoint")
+	})
+
+	t.Run("it observes the duration of the HTTP handler", func(t *testing.T) {
+		assert.True(t, histo.observeCalled)
+	})
 }
