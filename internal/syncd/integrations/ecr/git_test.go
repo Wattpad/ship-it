ef0208bba14820581143a055c11685c6847fbff5
Ethan Peterson <ethan.peterson@wattpad.com>
2019-08-07 12:41:29 -0400

    move test yaml to file where its used

--- internal/syncd/integrations/ecr/git_test.go
+++ internal/syncd/integrations/ecr/git_test.go
@@ -8,6 +8,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const inputYaml = `apiVersion: helmreleases.k8s.wattpad.com/v1alpha1
+kind: HelmRelease
+metadata:
+  creationTimestamp: null
+  name: example-microservice
+spec:
+  chart:
+    path: microservice
+    repository: wattpad.s3.amazonaws.com/helm-charts
+    revision: HEAD
+  releaseName: example-release
+  values:
+    image:
+      repository: 723255503624.dkr.ecr.us-east-1.amazonaws.com/bar
+      tag: baz
+status: {}
+`
+
+const expectedYaml = `apiVersion: helmreleases.k8s.wattpad.com/v1alpha1
+kind: HelmRelease
+metadata:
+  creationTimestamp: null
+  name: example-microservice
+spec:
+  chart:
+    path: microservice
+    repository: wattpad.s3.amazonaws.com/helm-charts
+    revision: HEAD
+  releaseName: example-release
+  values:
+    image:
+      repository: 723255503624.dkr.ecr.us-east-1.amazonaws.com/bar
+      tag: 78bc9ccf64eb838c6a0e0492ded722274925e2bd
+status: {}
+`
+
 func TestTransformBytes(t *testing.T) {
 	inputImage := &internal.Image{
 		Registry:   "723255503624.dkr.ecr.us-east-1.amazonaws.com",
