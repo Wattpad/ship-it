f66fa024dec71187ab56f224f7be1af9b81e6f12
Chris Dueck <chris.dueck@wattpad.com>
2019-08-21 12:16:17 -0400

    vendorless docker build
    * `go mod download` before introducing source file dependencies, so that
      source code changes don't cause expensive re-vendoring of dependencies
    
    * finer grain COPY directives so that the image depends on only the
      source files it needs
    
    * git/docker ignore the vendor dir and cache it in travis, for
      development we can cache locally

--- vendor/github.com/Wattpad/sqsconsumer/gen_mocks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p mock
-mockgen -destination mock/sqs_interface_mock.go -package mock github.com/Wattpad/sqsconsumer SQSAPI
--- vendor/github.com/Wattpad/sqsconsumer/middleware/log.go
+++
@@ -1,8 +0,0 @@
-package middleware
-
-import (
-	"log"
-	"os"
-)
-
-var stdoutLog = log.New(os.Stdout, "", log.LstdFlags)
