b9695fe3831763c628b6de75a1c51346ed90d806
Chris Dueck <chris.dueck@wattpad.com>
2019-12-09 12:15:41 -0500

    Handle "no affected registered releases" edge case in chart editor
    If a new image is pushed but there are no registered releases that use
    the image, the chart editor can exit early with a meaningful error
    message. The error message can be logged so we can see what syncd is
    doing even when there aren't many services registered.

--- internal/syncd/integrations/ecr/listener_test.go
+++ internal/syncd/integrations/ecr/listener_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/metrics/discard"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -71,9 +72,44 @@ func TestECRHandler(t *testing.T) {
 	"repositoryName": "monolith-php", 
 	"tag": "78bc9ccf64eb838c6a0e0492ded722274925e2bd",
 	"registryId": "723255503624"
-}
-`
+}`
 	err = testListener.handler(mockReconciler)(context.Background(), inputJSON)
 	assert.NoError(t, err)
 	mockReconciler.AssertExpectations(t)
+}
+
+type mockLogger struct {
+	mock.Mock
+}
+
+func (m *mockLogger) Log(kvs ...interface{}) error {
+	args := m.Called(kvs...)
+	return args.Error(0)
+}
+
+func TestReconcilerNoRegisteredReleasesAffected(t *testing.T) {
+	testErr := errors.Wrap(errNoRegisteredReleasesAffected, "wrapping with test context")
+
+	mockLogger := new(mockLogger)
+	mockLogger.On("Log", mock.AnythingOfType("string"), testErr.Error()).Return(nil)
+
+	testListener := &ImageListener{
+		logger: mockLogger,
+		timer:  discard.NewHistogram(),
+	}
+
+	inputJSON := `
+{
+	"eventTime": "2019-07-11T14:19:59Z", 
+	"repositoryName": "monolith-php", 
+	"tag": "78bc9ccf64eb838c6a0e0492ded722274925e2bd",
+	"registryId": "723255503624"
+}`
+
+	mockReconciler := new(MockReconciler)
+	mockReconciler.On("Reconcile", mock.Anything, mock.Anything).Return(testErr)
+
+	err := testListener.handler(mockReconciler)(context.Background(), inputJSON)
+	assert.NoError(t, err)
+	mockLogger.AssertExpectations(t)
 }
